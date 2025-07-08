62222100709308da4ab4b8dd4d532cf097b4321e
LeandroUTNFRA <leandroes2403@gmail.com>
2025-07-08 13:13:04 -0300

    correccion

--- js/main.js
+++ js/main.js
@@ -1,45 +1,18 @@
-//================= ARRAY DE OBJETOS=============
-//Punto 1 
-
 async function cargarProductosDesdeAPI() {
     try {
         const response = await fetch("http://localhost:3000/api/productos");
         const data = await response.json();
 
-        productos = data.payload; // ✅ guardás los productos globalmente
+        productos = data.payload; 
 
-        mostrarProductos(productos); // mostrás normalmente
+        mostrarProductos(productos); 
     } catch (error) {
         console.error("Error al cargar productos:", error);
     }
 }
 
 let productos = [];
-// ===================MOSTRAR DATOS DEL ALUMNO=============
-// punto 2
-
-/*En este Punto se pide mostrar de forma visible lo que son mis datos,asique 
-lo que hice fue definir un objeto alumno con los datos basicos y ya despues mostrarlo con un  `console.log`. Tambien 
-lo muestro en el DOM seleccionando la clase `.nombreAlumno` 
-function imprimirDatosAlumno() {
-  const alumno = {
-    dni: 46363895,
-    nombre: "Leandro",
-    apellido: "Escobar"
-  };
-
-  console.log(`Alumno: ${alumno.nombre} ${alumno.apellido} - DNI: ${alumno.dni}`);
-
-  const nombreAlumnoDiv = document.querySelector(".nombreAlumno");
-  if (nombreAlumnoDiv) {
-    nombreAlumnoDiv.textContent = `${alumno.nombre} ${alumno.apellido}`;
-  }
-}
-
-*/
-
 //====================MOSTRAR PRODUCTO====================
-//punto 3
 /* En este punto se pedia imprimir los productos(frutas),
 lo que hice fue recorrer el array de objetos con un forEach y dibujarlo en el DOM */
 function mostrarProductos(array){
@@ -61,7 +34,6 @@ function mostrarProductos(array){
 }
 
 // =============================== FILTRAR PRODUCTOS =============================== 
-// Punto 4
 /* En este punto se pide Filtrar segun la busqueda. Para eso, primero seleccionamos el input de busqueda por su ID y le asigne
  un evento `keyup`, que se dispara cada vez que el usuario presiona una tecla. y con el filter creo un nuevo array nuevo el cual lo muestro al final */
 
@@ -76,10 +48,6 @@ function filtrarProductos() {
 }
 
 //=========================FUNCIONALIDAD CARRITO============================
-//Punto 5
-/* En este punto se muestra lo que es el circuito del carrito
-    tanto como mostrar datos agregar y eliminar, Tambien inicialize un array carrito en el cual se va almacenar los 
-    productos  */
 function mostrarCarrito(array){
     const itemsCarrito = document.getElementById("items-carrito");
     let htmlCarrito = "";
@@ -108,7 +76,6 @@ function agregarCarritos(id) {
     }
 }
 
-
 function eliminarProducto(id){
     const index = carrito.findIndex(producto => producto.id === id);
     if (index !== -1) {
@@ -119,12 +86,7 @@ function eliminarProducto(id){
     }
 }
 
-//====================ACTUALIZAR PRECIO Y CONTADOR HEADER==================
-//Punto 7
-function actualizarContadorCarrito() {
-    const contador = document.getElementById("contador-carrito");
-    contador.textContent = carrito.length;
-}
+//====================ACTUALIZAR PRECIO==================
 function actualizarPrecioTotal() {
     let total = carrito.reduce((acum, producto) => acum + producto.precio, 0);
     const spanTotal = document.getElementById("precio-total");
@@ -134,7 +96,6 @@ console.log("Total calculado:", total);
 }
 
 //================= ORDENAMIENTO DE PRODUCTOS SEGUN EL PARAMETRO==============
-//Punto 8
 /*En este punto pide agregar botones para ordenar los botones entonces usando un sort lo ordeno segun 
 el criterio y finalmete uso un evento listener para cuando el usuario haga click en los botones*/ 
 function ordenamiento(criterio) {
@@ -173,7 +134,6 @@ function vaciarCarrito() {
   }
 }
 async function init(){
-    //sessionStorage.removeItem("nombreUsuario");
     verificarNombreUsuario();
     
     //imprimirDatosAlumno();
@@ -193,5 +153,3 @@ async function init(){
 }
 
 init();
-
-console.log("Productos recibidos desde API:", productos);
